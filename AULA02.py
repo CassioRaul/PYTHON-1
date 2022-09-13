168d7f29ffc1abccf4170062ce7ccf75a0e2e740
Cássio Raul <cassioraulcastro@gmail.com>
2022-09-12 21:05:11 -0300

    primeiro teste

---
+++ AULA02.py
@@ -0,0 +1,3 @@
+a = input('Qual a seu nome: ')
+b = input('Qual a sua idade: ')
+print(a, b)
---
+++ TESTE NOME IDADE.py
@@ -0,0 +1,7 @@
+a = input('Qual seu nome: ')
+b = input('Qual a sua idade: ')
+
+if b < 18:
+    print('VOCÊ É MENOR DE IDADE!')
+else:
+    print('SEU NOME É, ',a, ' E SUA IDADE É,',b, '.')
